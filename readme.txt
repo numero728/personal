717ac1fc974d08bf912f49d77c716a44b9ef8180
numero728 <numero728@naver.com>
2020-12-12 11:17:19 +0900

    cron

--- readme.txt
+++ readme.txt
@@ -8,3 +8,24 @@
 - key pair 이름: miniproject_key
 - DNS 이름: Configure
 - IP: 40.76.38.92
+
+- DB 인스턴스_식별자: personaldb
+- 사용자 이름: admin
+- pw: pnudb960726!
+- host: cepsu2i8bkn5.ap-northeast-2.rds.amazonaws.com
+# 서버 최초 배포 후 apache2 default page 설정
+# ssh 접속 후
+1) /etc/apache2/sites_available 디렉토리로 이동
+2) sudo a2dissite 000-default.conf
+3) sudo systemctl reload apache2
+
+# 크롬 브라우저 설치
+1. sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+2. dpkg로 deb 파일 install
+3. dpkg 명령 수행 위한 requirements 충족 필요
+3.1. dppk 명령 수행 이후 -> dependencies 언급됨
+3.2. sudo apt-get -f install
+
+# 크롬 드라이버 설치
+1. wget -N https://chromedriver.storage.googleapis.com/87.0.4280.88/chromedriver_linux64.zip
+2. unzip으로 압축 해제
---
+++ scrap/cron_test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+import json
+import os
+
+url='https://www.naver.com/'
+driver=webdriver.Chrome('chromedriver')
+driver.get(url)
+got=driver.find_element_by_css_selector('#themecast [aria-selected="true"]')
+got=str(got)
+got=[got]
+data=pd.DataFrame(got)
+print(data)
+DB_env=json.load(open(str(os.path.abspath(os.getcwd()))+'/database.json'))
+DB_url=f"{DB_env['DIALECT']}+{DB_env['DB_DRIVER']}://{DB_env['DB_USERMAME']}:{DB_env['DB_PASSWORD']}@{DB_env['DB_HOST']}:{DB_env['DB_PORT']}/{DB_env['DB_DATABASE']}"
+print(DB_url)
+engine=create_engine(DB_url)
+conn=engine.connect()
+data.to_sql('cron_test',conn,if_exists='append',index=False)
+driver.close()
+driver.quit()
