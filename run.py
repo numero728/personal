75b4568799ead11471186e1b9cbd56ae0a5e1598
numero728 <numero728@naver.com>
2020-12-16 09:11:40 +0900

    ad

--- run.py
+++ run.py
@@ -1,41 +1,65 @@
+# 모듈 import
+
+
 from flask import Flask, render_template, session, make_response, url_for, redirect, request, jsonify
 from flask_socketio import SocketIO, emit
 import re
 import time
 from DB.db_query import *
 
+
+
+# app 생성 및 환경설정
+
+
 app=Flask(__name__)
+app.config['SECRET_KEY']='miniproject'
+users=[]
+
+# socketio 객체 생성
+
 
 socketio=SocketIO(app,cors_allowed_origins='*', pingInterval=600000,pingTimeout=600000,async_mode='threading', manage_session=False)
+
+
+
 msg_pack=[]
 
+dir(socketio)
+
+
+
+# socketio 통신 관련
 if True:
+
+  # 2. 유저 접속 수신
   @socketio.on('connect')
   def connect():
     print('유저 접속')
 
-  @socketio.on('client_send_name')
-  def client_send_name_handler(data):
-    print(dir(data))
-    client_name=data['client_name']
-    msg=f'{client_name}님이 입장하셨습니다.'
+
+
+  # 4. 유저 송신한 닉네임 수신 및 시스템 메시지 송신
+  @socketio.on('username')
+  def username(data):
+    username=data['username']
+    msg=f'{username}님이 입장하셨습니다.'
     emit('system_msg',{'user':'admin','msg':msg},broadcast=True)
-    # if len(msg_pack)>100:
-    #   msg_pack.pop(msg_pack[0])
-    #   msg_pack.append({'user':'awedmin','msg':msg})
-    # else:
-    #   msg_pack.append({'user':'admin','msg':msg}) 
-    
-  @socketio.on('client_send_msg')
-  def client_send_msg_handler(data):
-    client_name=data['client_name']
-    msg=data['client_msg']
-    emit('server_send_msg',{'user':client_name,'msg':msg, 'stock_code':'Null'},broadcast=True)
-    # if len(msg_pack)>100:
-    #   msg_pack.pop(msg_pack[0])
-    #   msg_pack.append({'user':client_name,'msg':msg})
-    # else:
-    #   msg_pack.append({'user':client_name,'msg':msg})
+
+
+  # 7. 유저 송신한 메시지 수신 및 중계
+  @socketio.on('c_send_msg')
+  def c_send_msg(data):
+    username=data['username']
+    msg=data['msg']
+    emit('s_send_msg',{'user':username,'msg':msg},broadcast=True)
+
+
+  # 9. 유저 이탈
+  @socketio.on('disconnect')
+  def disconnect():
+    print('유저 이탈')
+
 
 if True:
   @app.route('/')
@@ -44,37 +68,42 @@ if True:
 
   @app.route('/news')
   def news():
-      # pageNo이 있는 경우;
-      # 주어진 pageNo으로 쿼리 수행한 다음
-      # 결과 반환
-      print(request.args.get('pageNo'))
-      if 'pageNo' in request.args:
-        pageNo=request.args.get('pageNo')
-        data=news_query(pageNo)
-        res={
-          'pageNo':pageNo,
-          'result':data
-          }
-        return jsonify(res)
-      else:
-        pageNo=1
-        data=news_query(pageNo)
-        return render_template('news.html', result=data, pageNo=pageNo)
+      data=news_query()
+      return render_template('news.html', result=data)
 
   @app.route('/exchange')
   def exchange():
       data=exch_query()
+      data_body=[]
+      for d in data:
+        d.pop('info_link')
+        data_body.append(d)
+      data=data_body
       return render_template('exchange.html', result=data)
 
   @app.route('/index')
   def index():
-      data=index_query()
-      return render_template('index.html', result=data)
+    data=index_query()
+    list(data[0].keys())
+    data_body=[]
+    for d in data:
+      k=list(d.keys())
+      v=list(d.values())
+      k=k[0:9]
+      v=v[0:9]
+      kv=dict(zip(k,v))
+      data_body.append(kv)
+    data=data_body
+    return render_template('index.html', result=data)
 
   @app.route('/youtube')
   def youtube():
       data=youtube_query()
       return render_template('youtube.html', result=data)
+
+  @app.route('/test')
+  def test():
+      return render_template('test.html')
 
 
   @app.route('/chat')
@@ -102,6 +131,8 @@ if True:
       return render_template('chat.html', reconnect='True')
     else:
       return render_template('chat.html', reconnect='False')
+
+  
 
 
 if __name__=='__main__':
